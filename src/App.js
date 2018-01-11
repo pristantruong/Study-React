6f30a8c21c98baa9ebaf0eb1149b88a591d9e142
Sang <pristan106@gmail.com>
2018-01-12 01:37:47 +0700

    JSX, Object, Variable

--- src/App.js
+++ src/App.js
@@ -4,20 +4,44 @@ import './App.css';
 
 class App extends Component {
   render() {
+  	var a = 106;
+  	var b = 1996;
+  	var name = 'sang';
+  	//Object
+  	var product = {
+  		id : 1, 
+  		name : 'Iphone X',
+  		price : '$1500'
+  	}
     return (
- 		<nav className="navbar navbar-inverse">
- 			<div className="container-fluid">
- 				<a className="navbar-brand" href="#">Title</a>
- 				<ul className="nav navbar-nav">
- 					<li className="active">
- 						<a href="#">Home</a>
- 					</li>
- 					<li>
- 						<a href="#">Link</a>
- 					</li>
- 				</ul>
- 			</div>
- 		</nav>
+ 		<div>
+ 			<nav className="navbar navbar-inverse">
+ 				<div className="container-fluid">
+ 					<a className="navbar-brand" href="#">Title</a>
+ 					<ul className="nav navbar-nav">
+ 						<li className="active">
+ 							<a href="#">Home</a>
+ 						</li>
+ 						<li>
+ 							<a href="#">Link</a>
+ 						</li>
+ 					</ul>
+ 				</div>
+ 			</nav>
+ 			<h2>
+ 				a: {a} <br/>
+ 				b: {b} <br/>
+ 				a + b = {a + b}
+ 			</h2>
+ 			<h3>
+ 				name : {name}
+ 			</h3>
+ 			<h3>
+ 				id : {product.id} <br/>
+ 				name: {product.name} <br/>
+ 				price: {product.price}
+ 			</h3>
+ 		</div>
     );
   }
 }
