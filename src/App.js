81f0b1c6ea38b1d503e50593856538e4509a1acf
Sang <pristan106@gmail.com>
2018-01-17 11:08:38 +0700

    Teamplate & Components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import TaskForm from './components/TaskForm';
+import SearchSort from './components/SearchSort';
+import TaskList from './components/TaskList';
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+
+	render() {
+		return (
+			<div className="container">
+				<div className="text-center">
+					<h1>Task Manager</h1><hr />
+				</div>
+				<div className="row">
+
+					<div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+						{/* Form */}
+						<TaskForm />
+					</div>
+					<div className="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+
+						<button type="button" className="btn btn-primary">
+							<span className="fa fa-plus mr-5"></span>Add Task</button>
+						{/* Search and Sort */}
+
+						{/* Search */}
+						<SearchSort />
+						<div className="row mt-15">
+							<div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+								<TaskList />
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+
+		);
+	}
 }
 
 export default App;
---
+++ src/components/TaskForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+class TaskForm extends Component {
+    render() {
+        return (
+            <div className="panel panel-primary">
+                <div className="panel-heading">
+                    <h3 className="panel-title">Add Task
+							  		<span className="fa - fa-times-circle text-right"></span>
+                    </h3>
+                </div>
+                <div className="panel-body">
+                    <form>
+                        <div className="form-group">
+                            <label >Name :</label>
+                            <input type="text"
+                                className="form-control"
+                                name="name" />
+                        </div>
+                        <label>Status</label>
+                        <select className="form-control"
+                            name="status">
+                            <option value={true}>Active</option>
+                            <option value={false}>Invisible</option>
+                        </select><br />
+                        <div className="text-center">
+
+                            <button type="button" className="btn btn-success">
+                                <span className="fa fa-plus mr-5"></span>Save
+										</button>&nbsp;
+
+										<button type="button" className="btn btn-danger">
+                                <span className="fa fa-plus mr-5"></span>Cancel
+										</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+
+
+        );
+    }
+}
+
+export default TaskForm;
